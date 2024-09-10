6615e01980ec6c79b7768cea32068d4013b5aa5b
Lucas Henrique Correia Carvalho <20221041110021@ifrn.local>
2024-09-10 10:43:45 -0300

    finalizado corretamente

--- atividade 4/questão 1/script.js
+++ atividade 4/questão 1/script.js
@@ -1,3 +1,4 @@
+
 function changeColors_blue() {
     var elements = document.getElementsByClassName("blue");
     for (var i = 0; i < elements.length; i++) {
@@ -12,28 +13,32 @@ function changeColors_green() {
     }
 }
 
-function changeColors_green() {
-    var elements = document.getElementsByClassName("green");
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].style.background = "green";
-    }
-}
-
-function removeColors() {
+function removeColors_blue() {
     var blueElements = document.getElementsByClassName("blue");
-    var greenElements = document.getElementsByClassName("green");
 
     for (var i = 0; i < blueElements.length; i++) {
         blueElements[i].style.background = "";
     }
+}
+
+function removeColors_green() {
+    var greenElements = document.getElementsByClassName("green");
 
     for (var i = 0; i < greenElements.length; i++) {
         greenElements[i].style.background = "";
     }
 }
 
-document.querySelectorAll('td').forEach(td => {
-    td.addEventListener('click', function() {
-        removeColors();
+var blueElements = document.getElementsByClassName("a")
+for (var i = 0; i < blueElements.length; i++) {
+    blueElements[i].addEventListener('click', function() {
+        removeColors_blue();
     });
-});
+}
+
+var greenElements = document.getElementsByClassName("b")
+for (var i = 0; i < greenElements.length; i++) {
+    greenElements[i].addEventListener('click', function() {
+        removeColors_green();
+    });
+}
